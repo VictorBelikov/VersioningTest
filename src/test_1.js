3eeb468227af8b96594c4d16c90d762db781b7cb
Victor Belikov <v1ctor.belikov@gmail.com>
2023-04-04 11:49:19 +0300

    test2

--- src/test_1.js
+++ src/test_1.js
@@ -1,7 +1,4 @@
 const test_1 = () => {
   console.log("Hello, world!");
   console.log("Hello, world!");
-  console.log("Hello, world!");
-  console.log("Hello, world!");
-  console.log("Hello, world!");
 };
